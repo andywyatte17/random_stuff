bd2c71fb4202cc26e6fca3bb32bb92e39daae11d
Andy Wyatt <andyw@emnetsoftware.com>
2013-09-25 09:07:16 +0100

    Added html code and javascript.

---
+++ maps/js/detectMoveOrDrag.js
@@ -0,0 +1,15 @@
+// Idea from http://jsbin.com/eyotuz/3/edit,
+//           http://stackoverflow.com/a/16423486/5056
+//
+
+var isMouseMoveNotDrag = false
+
+$(document).on('mousemove', detectMouse);
+
+function detectMouse(e) {
+  if(e.which || e.button)
+    return;
+  isMouseMove = true
+  $('p.mouseOrNot').text("Mouse Detected!");
+  $(document).off('mousemove', detectMouse);
+}
