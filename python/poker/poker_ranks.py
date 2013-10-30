b5f522326577694ce595654edc7458b9b8fd978a
Andy Wyatt <andywyatte17@gmail.com>
2013-10-30 23:44:01 +0000

    Full implementation of poker_rank is_... functions; not well tested!

--- python/poker/poker_ranks.py
+++ python/poker/poker_ranks.py
@@ -20,6 +20,18 @@ def card_to_high_value(card):
   if card=='A': return 14
   return int(card)
 
+def card_to_high_value_ace_low(card):
+  if card=='J': return 11
+  if card=='Q': return 12
+  if card=='K': return 13
+  if card=='A': return 1
+  return int(card)
+
+def distance(card_tup_1, card_tup_2):
+  v1 = ( card_to_high_value(card_tup_1[0]), card_to_high_value_ace_low(card_tup_1[0]) )
+  v2 = ( card_to_high_value(card_tup_2[0]), card_to_high_value_ace_low(card_tup_2[0]) )
+  return min( abs(v1[0]-v2[0]), abs(v1[0]-v2[1]), abs(v1[1]-v2[0]), abs(v1[1]-v2[1]) )
+
 def card_to_high_value_x(card, x):
   if card==x: return 1000
   if card=='J': return 11
@@ -42,32 +54,65 @@ def group_for_pairs(combin):
   runs = [len(list(group)) for key, group in groupby(combinS, key=lambda x:x[0])]
   return ( runs, combinS )
 
-def is_four_of_kind(combin):
+def is_four_of_kind(combin, g4p):
+  if g4p[0]==[4,1]:
+    rv = sorted(combin, key = lambda x : card_to_high_value_x(x[0], g4p[1][3][0]), reverse=True )
+    # pprint.pprint( ("is_two_pairs", rv) )
+    return rv
+  if g4p[0]==[1,4]:
+    rv = sorted(combin, key = lambda x : card_to_high_value_x(x[0], g4p[1][4][0]), reverse=True )
+    # pprint.pprint( ("is_two_pairs", rv) )
+    return rv
+
+def is_full_house(combin, g4p):
+  if g4p[0]==[3,2]:
+    rv = sorted(combin, key = lambda x : card_to_high_value_x(x[0], g4p[1][0][0]), reverse=True )
+    #pprint.pprint( ("is_full_house", rv) )
+    return rv
+  if g4p[0]==[2,3]:
+    rv = sorted(combin, key = lambda x : card_to_high_value_x(x[0], g4p[1][2][0]), reverse=True )
+    #pprint.pprint( ("is_full_house", rv) )
+    return rv
+
+def is_flush(combin, g4p):
+  for i in range(1,5):
+    if combin[i-1][1]!=combin[i][1]:
+      return None
+  #pprint.pprint( ("flush!", g4p[1]) )
+  return g4p[1]
+
+def is_straight(combin, g4p):
+  combinS = g4p[1]
+  if distance(combinS[0], combinS[4])>5:
+    return None
+  for i in range(1,5):
+    if distance(combinS[i-1], combinS[i])!=1:
+      return None
+  #pprint.pprint( ("straight!", g4p[1]) )
   return None
 
-def is_full_house(combin):
-  return None
-
-def is_three_of_kind(combin):
-  return None
-
-def is_flush(combin):
-  return None
-
-def is_straight(combin):
-  return None
-
-def is_straight_flush(combin):
-  straight = is_straight(combin)
-  if straight and is_flush(combin):
+def is_straight_flush(combin, g4p):
+  straight = is_straight(combin, g4p)
+  if straight and is_flush(combin, g4p):
     return straight
   return None
 
-def is_three_of_kind(combin):
+def is_three_of_kind(combin, g4p):
+  if g4p[0]==[3,1,1]:
+    rv = sorted(combin, key = lambda x : card_to_high_value_x(x[0], g4p[1][2][0]), reverse=True )
+    # pprint.pprint( ("is_two_pairs", rv) )
+    return rv
+  if g4p[0]==[1,3,1]:
+    rv = sorted(combin, key = lambda x : card_to_high_value_x(x[0], g4p[1][3][0]), reverse=True )
+    # pprint.pprint( ("is_two_pairs", rv) )
+    return rv
+  if g4p[0]==[1,1,3]:
+    rv = sorted(combin, key = lambda x : card_to_high_value_x(x[0], g4p[1][4][0]), reverse=True )
+    # pprint.pprint( ("is_two_pairs", rv) )
+    return rv
   return None
 
-def is_two_pairs(combin):
-  g4p = group_for_pairs(combin)
+def is_two_pairs(combin, g4p):
   if g4p[0]==[1,2,2]:
     rv = sorted(combin, key = lambda x : card_to_high_value_xy(x[0], g4p[1][2][0], g4p[1][4][0]), reverse=True )
     # pprint.pprint( ("is_two_pairs", rv) )
@@ -82,7 +127,23 @@ def is_two_pairs(combin):
     return rv
   return None
 
-def is_pair(combin):
+def is_pair(combin, g4p):
+  if g4p[0]==[2,1,1,1]:
+    rv = sorted(combin, key = lambda x : card_to_high_value_x(x[0], g4p[1][0][0]), reverse=True )
+    # pprint.pprint( ("is_two_pairs", rv) )
+    return rv
+  if g4p[0]==[1,2,1,1]:
+    rv = sorted(combin, key = lambda x : card_to_high_value_x(x[0], g4p[1][1][0]), reverse=True )
+    # pprint.pprint( ("is_two_pairs", rv) )
+    return rv
+  if g4p[0]==[1,1,2,1]:
+    rv = sorted(combin, key = lambda x : card_to_high_value_x(x[0], g4p[1][2][0]), reverse=True )
+    # pprint.pprint( ("is_two_pairs", rv) )
+    return rv
+  if g4p[0]==[1,1,1,2]:
+    rv = sorted(combin, key = lambda x : card_to_high_value_x(x[0], g4p[1][3][0]), reverse=True )
+    # pprint.pprint( ("is_two_pairs", rv) )
+    return rv
   return None
 
 def is_high_card(combin):
@@ -91,32 +152,24 @@ def is_high_card(combin):
   return rv
 
 def find_poker_value(combin):
-  sorted = is_straight_flush(combin)
+  g4p = group_for_pairs(combin)
+  sorted = is_straight_flush(combin, g4p)
   if sorted: return ( 9, "Straight Flush", sorted )
-
-  sorted = is_four_of_kind(combin)
+  sorted = is_four_of_kind(combin, g4p)
   if sorted: return ( 8, "Four of a kind", sorted )
-
-  sorted = is_full_house(combin)
+  sorted = is_full_house(combin, g4p)
   if sorted: return ( 7, "Full House", sorted )
-
-  sorted = is_four_of_kind(combin)
+  sorted = is_four_of_kind(combin, g4p)
   if sorted: return ( 6, "Four of a kind", sorted )
-
-  sorted = is_flush(combin)
+  sorted = is_flush(combin, g4p)
   if sorted: return ( 5, "Flush", sorted )
-
-  sorted = is_straight(combin)
+  sorted = is_straight(combin, g4p)
   if sorted: return ( 4, "Straight", sorted )
-
-  sorted = is_three_of_kind(combin)
+  sorted = is_three_of_kind(combin, g4p)
   if sorted: return ( 3, "Three of a kind", sorted )
-
-  sorted = is_two_pairs(combin)
+  sorted = is_two_pairs(combin, g4p)
   if sorted: return ( 2, "Two pairs", sorted )
-
-  sorted = is_pair(combin)
+  sorted = is_pair(combin, g4p)
   if sorted: return ( 1, "Pair", sorted )
-
   sorted = is_high_card(combin)
   if sorted: return ( 0, "High card", sorted )
--- python/poker/poker_sim.py
+++ python/poker/poker_sim.py
@@ -11,29 +11,44 @@ def make_pack():
   return pack
 
 def card_value_less(card_pair1, card_pair2):
-  # pprint.pprint( ("card_pair1", card_pair1 ) )
-  # pprint.pprint( ("card_pair2", card_pair2 ) )
+  #pprint.pprint( ("card_pair1", card_pair1 ) )
+  #pprint.pprint( ("card_pair2", card_pair2 ) )
   return card_to_high_value( card_pair1[0] ) < card_to_high_value( card_pair2[0] )
 
-def compare_hand_tuple_less(hand_tuple1, hand_tuple2):
-  # pprint.pprint( ("hand_tuple1", hand_tuple1 ) )
-  # pprint.pprint( ("hand_tuple2", hand_tuple2 ) )
+def compare_hand_tuple_less_0(hand_tuple1, hand_tuple2):
   if hand_tuple1[0]<hand_tuple2[0]:
     return True
   if hand_tuple2[0]<hand_tuple1[0]:
     return False
   for i in range(0,5):
     if card_value_less( hand_tuple1[2][i], hand_tuple2[2][i] ):
-      return False
-    if card_value_less( hand_tuple2[2][i], hand_tuple1[2][i] ):
       return True
+    if card_value_less( hand_tuple2[2][i], hand_tuple1[2][i] ):
+      return False
   return False
+
+def compare_hand_tuple_less(hand_tuple1, hand_tuple2):
+  res = compare_hand_tuple_less_0(hand_tuple1, hand_tuple2)
+  #if hand_tuple1[0]==hand_tuple2[0]:
+  #  pprint.pprint( (hand_tuple1,hand_tuple2) )
+  #  print "a less-than b?",res
+  #  raw_input()
+  return res
 
 def draw_and_remove(pack):
   shuffle( pack )
   draw = pack[0]
   pack.remove(draw)
   return draw
+
+def draw_and_remove_value(pack, val):
+  shuffle( pack )
+  for i in pack:
+    if i[0]==val:
+      draw = i
+      pack.remove(i)
+      return draw
+  return None
 
 '''
   returns tuple( hand_type, hand_name, card_value_high_to_low )
@@ -60,7 +75,7 @@ def hand_poker_value(hand, pool):
     tup = find_poker_value(combin)
     if not hand_tuple or compare_hand_tuple_less(hand_tuple, tup):
       hand_tuple = tup
-  return tup
+  return hand_tuple
 
 def winning_hand(pack_in, my_hand, pool, no_players):
   if not pool:
@@ -73,28 +88,47 @@ def winning_hand(pack_in, my_hand, pool, no_players):
   # pprint.pprint( other_hands )
   # print "Pool"
   # pprint.pprint( pool )
-  my_hand_value = hand_poker_value(my_hand, pool)
+  my_hand_value = ( my_hand, hand_poker_value(my_hand, pool) )
   other_hands = list()
   for i in range(1, no_players):
     other_hand = ( draw_and_remove(pack), draw_and_remove(pack) )
     other_hands.append( ( other_hand, hand_poker_value(other_hand, pool) ) )
-  # Compare
-  # pprint.pprint( other_hands )
-  return ( my_hand, hand_poker_value(my_hand, pool) )
+    # pprint.pprint( ("other_hands", other_hands ) )
+  best_hand_value = my_hand_value
+  for other_hand in other_hands:
+    compLess = compare_hand_tuple_less(best_hand_value[1], other_hand[1])
+    # pprint.pprint( ("my_h v other_hand", best_hand_value[1], other_hand[1], compLess ) )
+    if compLess:
+      best_hand_value = other_hand
+  return ( best_hand_value, my_hand_value )
 
 # pprint.pprint(pack)
 
 pack = make_pack()
 shuffle( pack )
-pprint.pprint( pack )
+# pprint.pprint( pack )
 
 print "Texas Hold 'Em Simulator"
-no_players = 4
+no_players = 8
 print "Number of players =", no_players
 
-my_hand = ( draw_and_remove(pack), draw_and_remove(pack) )
+# my_hand = ( draw_and_remove(pack), draw_and_remove(pack)
+my_hand = ( draw_and_remove_value(pack, 'A'), draw_and_remove_value(pack, 'A') )
 print "Your hand:"
 pprint.pprint( my_hand )
 print "Number remains:", len(pack)
-win_hand = winning_hand(pack, my_hand, None, no_players)
-pprint.pprint( ("win_hand", win_hand, win_hand==my_hand) )
+
+win_count = 0
+lose_count = 0
+end = 1024*16-1
+for i in range(0, end+1):
+  win_hand = winning_hand(pack, my_hand, None, no_players)
+  #print "<"
+  #pprint.pprint( win_hand )
+  #print ">"
+  if win_hand[0][0]==my_hand:
+    win_count = win_count + 1
+  else:
+    lose_count = lose_count + 1
+  if i==end or ((i%128)==127):
+    pprint.pprint( ("win v loss", win_count, lose_count, "{0}%".format(100.0*win_count / (win_count+lose_count))) )
