2670bbe503f0834269ae53875b1ec5eca84a5a29
Andy Wyatt <andywyatte17@gmail.com>
2014-09-02 14:05:15 +0100

    Start of GuessWho python project.

---
+++ python/GuessWho/GuessWho.py
@@ -0,0 +1,42 @@
+#!/bin/python
+
+import argparse
+import StringIO
+import re
+
+def parse_characters():
+    import characters
+    rv = dict()
+    x = StringIO.StringIO(characters.people)
+    for line in x:
+        rx = re.match(R' *([A-Za-z]*) *- *([A-Za-z])?,?([A-Za-z])?,?([A-Za-z])?,?([A-Za-z])?,?([A-Za-z])?,?([A-Za-z])?,?', line)
+        if rx:
+            c = 0
+            name = ''
+            for x in rx.groups():
+                if c==0:
+                    name = x
+                    rv[name] = set()
+                else:
+                    if x:
+                        rv[name].add(characters.mapping[x])
+                c += 1
+    return rv
+
+def dump_characters():
+    import pprint
+    pprint.pprint(parse_characters())
+    
+def main():
+    pass
+
+if __name__=='__main__':
+    parser = argparse.ArgumentParser(description='Run the Guess Who game')
+    parser.add_argument('--dump_characters', dest='dump_characters', action='store_const',
+                        const=sum, default=False,
+                        help='show information about the characters and exit.')
+    args = parser.parse_args()
+    if args.dump_characters or True:
+        dump_characters()
+        exit(0)
+    main()
---
+++ python/GuessWho/characters.py
@@ -0,0 +1,51 @@
+#!/bin/python
+
+mapping = { 'k':'Black Hair', 'c':'Brown Hair', 'o':'Orange Hair',
+            'y':'Yellow Hair', 'w':'White Hair',
+            #
+            'B':'Blue Eyes', 
+            #
+            'h':'Wearing a Hat',
+            #
+            'C':'Has a Beard', 'M':'Has a Moustache',
+            #
+            'H':'Has a Big Nose',
+            #
+            'F':'Has a Bald Top',
+            #
+            'g':'Is Wearing Glasses',
+            #
+            'r':'Has Rosy Cheeks',
+            #
+            'f':'Is Female'
+          }
+
+people = \
+'''
+  Alex - k,M,
+  Anne - f,k,H,
+  Tom - g,k,F,B,
+  Max - M,c,H,
+  Richard - M,C,c,F,
+  Robert - c,H,r,B,
+  Bernard - c,H,
+  Maria - f,c,
+
+  Philip - C,k,r,
+  Joe - g,y,
+  George - w,
+  Sam - g,w,F,
+  Paul - g,w,
+  Susan - f,w,r,
+  Claire - f,g,o,
+  Frans - o,
+
+  Anita - f,y,r,B,
+  Peter - w,H,B,
+  David - C,y,
+  Charles - M,y,
+  Alfred - M,o,B,
+  Bill - C,o,F,r,
+  Herman - o,H,F,
+  Eric - y,
+'''
