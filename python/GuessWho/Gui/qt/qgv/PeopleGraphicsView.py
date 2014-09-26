4f30d3c7e46db7829e75b9d450eed9d3ec3b2bd2
Andy Wyatt <andywyatte17@gmail.com>
2014-09-26 09:02:25 +0100

    GuessWho : Further work on PeopleGraphicsView

---
+++ python/GuessWho/Gui/qt/qgv/PeopleGraphicsView.py
@@ -0,0 +1,87 @@
+from PyQt4.QtCore import *
+from PyQt4.QtGui import *
+
+
+class MyImage(QGraphicsItem):
+    def __init__(self, name, imagePath, x=0, y=0):
+        super(MyImage, self).__init__()
+        self.imagePath = imagePath
+        self.image = None
+        self.name = name
+        self.pos = 0
+        self.x = x
+        self.y = y
+
+    def getWH(self):
+        return (75, 90)
+
+    def boundingRect(self):
+        W,H = self.getWH()
+        return QRectF(self.x*W, self.y*H, W, H)
+
+    def paint(self, painter, option, widget):
+        if self.image == None:
+            self.image = QImage(self.imagePath)
+
+        TEXTH = 15
+        W,H = self.getWH()
+        f = self.pos/100.0
+        f2 = self.pos/200.0
+        x0 = self.x * W
+        y0 = self.y * H
+        w1 = (W-4.0) / W
+        h1 = (H-4.0) / H
+        oldTransform = painter.worldTransform()
+        painter.setWorldTransform( QTransform(w1, 0, 0, h1 - f*h1, 2 + x0, 2 + y0 + f2 * (H-4.0) - (f2*TEXTH) ) )
+        painter.setRenderHint( QPainter.SmoothPixmapTransform, True )
+        painter.setOpacity( 1 - (f2/1.5) )
+        painter.drawImage( QRectF(0, 0, W, H-TEXTH), self.image )
+        painter.setWorldTransform(oldTransform)
+        painter.drawText( QRectF(x0, y0, W, H), Qt.AlignBottom | Qt.AlignHCenter, "Alex" )
+
+
+class MyGraphicsView(QGraphicsView):
+    def __init__(self, imagePathForName, parent):
+        super(MyGraphicsView, self).__init__(parent)
+        self.imagePathForName = imagePathForName
+        self.initUI()
+        
+    def initUI(self):
+        scene=QGraphicsScene()
+        pixMapItem = MyImage( "Alex", self.imagePathForName("Alex") )
+        scene.addItem( pixMapItem )
+        pixMapItem2 = MyImage( "Robert", self.imagePathForName("Robert"), x=0, y=1)
+        scene.addItem( pixMapItem2 )
+        self.shouldScaleDown = True
+        self.item = None
+        self.timer = None
+        self.setSceneRect(0, 0, 300, 400)    
+        self.setScene(scene)
+
+    def fadeOut(self, n):
+        self.item.pos = n
+        self.scene().invalidate( self.item.boundingRect() )
+
+    def fadeIn(self, n):
+        self.item.pos = 200 - n
+        self.scene().invalidate( self.item.boundingRect() )
+
+    def timerFinish(self):
+        self.timer = None
+
+    def mousePressEvent(self, event):
+        if self.timer:
+            return
+        self.item = self.itemAt(event.pos())
+        if not self.item:
+            return
+        timer = QTimeLine(500)
+        timer.finished.connect( self.timerFinish )
+        self.timer = timer
+        timer.setFrameRange(0, 200)
+        if self.shouldScaleDown:
+            timer.frameChanged.connect( self.fadeOut )
+        else:
+            timer.frameChanged.connect( self.fadeIn )
+        timer.start();
+        self.shouldScaleDown = not self.shouldScaleDown
--- python/GuessWho/Gui/qt/qgv/qgv.py
+++ python/GuessWho/Gui/qt/qgv/qgv.py
@@ -2,99 +2,26 @@
 # -*- coding: utf-8 -*-
 
 import sys
-from PyQt4 import QtGui, QtCore
 from PyQt4.QtCore import *
 from PyQt4.QtGui import *
-
-class MyImage(QGraphicsItem):
-    def __init__(self, x=0, y=0):
-        super(MyImage, self).__init__()
-        self.pos = 0
-        self.image = QImage(R"../../tiles/Max.jpg")
-        self.x = x
-        self.y = y
-
-    def getWH(self):
-        return (75, 90)
-
-    def boundingRect(self):
-        W,H = self.getWH()
-        return QRectF(self.x*W, self.y*H, W, H)
-
-    def paint(self, painter, option, widget):
-        W,H = self.getWH()
-        f = self.pos/100.0
-        f2 = self.pos/200.0
-        w = self.x * W
-        h = self.y * H
-        w1 = (W-4.0) / W
-        h1 = (H-4.0) / H
-        print w1, h1
-        painter.setWorldTransform( QTransform(w1, 0, 0, h1 - f*h1, 2 + w, 2 + h + f2 * (H-4.0) ) )
-        painter.setRenderHint( QPainter.SmoothPixmapTransform, True )
-        painter.setOpacity( 1 - (f2/2) )
-        painter.drawImage( QRectF(0, 0, W, H), self.image )
+from PeopleGraphicsView import *
 
 
-class MyGraphicsView(QGraphicsView):
-    def __init__(self, a, b):
-        super(MyGraphicsView, self).__init__(a, b)
-        self.shouldScaleDown = True
-        self.item = None
-        self.timer = None
-
-    def scaleDown(self, n):
-        self.item.pos = n
-        self.scene().invalidate()
-
-    def scaleUp(self, n):
-        self.item.pos = 200 - n
-        self.scene().invalidate()
-
-    def timerFinish(self):
-        self.timer = None
-
-    def mousePressEvent(self, event):
-        if self.timer:
-            return
-        self.item = self.itemAt(event.pos())
-        if not self.item:
-            return
-        timer = QtCore.QTimeLine(500)
-        timer.finished.connect( self.timerFinish )
-        self.timer = timer
-        timer.setFrameRange(0, 200)
-        if self.shouldScaleDown:
-            timer.frameChanged.connect( self.scaleDown )
-        else:
-            timer.frameChanged.connect( self.scaleUp )
-        timer.start();
-        self.shouldScaleDown = not self.shouldScaleDown
-
-class Example(QtGui.QWidget):
+class Example(QWidget):
     def __init__(self):
         super(Example, self).__init__()
         self.initUI()
 
     def initUI(self):
-        hbox=QtGui.QHBoxLayout()
-        leftpanel=QtGui.QFrame()
+        hbox=QHBoxLayout()
+        leftpanel=QFrame()
         leftpanel.setGeometry(0,0,300,400)
-        scene=QtGui.QGraphicsScene()
-        self.scene = scene  # save reference to scene, or it will be destroyed
-        pixMapItem = MyImage()
-        self.scene.addItem( pixMapItem )
-        pixMapItem2 = MyImage(x=0, y=1)
-        self.scene.addItem( pixMapItem2 )
-        view=MyGraphicsView(scene,leftpanel)
-        view.pixMapItem = pixMapItem
-        view.pixMapItem2 = pixMapItem2
-        view.setSceneRect(0,0,300,400)
-        pen=QtGui.QPen(QtCore.Qt.black,2)
+        view=MyGraphicsView( lambda x: "../../tiles/{}.jpg".format(x), leftpanel)
+        pen=QPen(Qt.black,2)
         hbox.addWidget(leftpanel)
-        rightpanel=QtGui.QFrame()
+        rightpanel=QFrame()
         hbox.addWidget(rightpanel)
-        szoveg=QtGui.QLabel(rightpanel)
+        szoveg=QLabel(rightpanel)
         szoveg.setText(u"Hello World!")
         self.setLayout(hbox)
         self.resize(500,500)
@@ -102,7 +29,7 @@ class Example(QtGui.QWidget):
         self.show()
 
 def main():
-    app = QtGui.QApplication(sys.argv)
+    app = QApplication(sys.argv)
     ex = Example()
     sys.exit(app.exec_())
 
