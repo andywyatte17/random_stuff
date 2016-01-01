7891fbe934141f64858d36a198b2d368448b65ce
Andy Wyatt <andywyatte17@gmail.com>
2016-01-01 23:03:28 +0000

    New: python > livedarts scraper project.

---
+++ python/livedarts/css.py
@@ -0,0 +1,112 @@
+style = """
+<style type="text/css">
+    html {
+        overflow: scroll;
+    }
+    body {
+        background: #000000;
+        font-family: Arial, Helvetica;
+    }
+    .button {
+  font: bold 11px Arial;
+  text-decoration: none;
+  background-color: #EEEEEE;
+  color: #333333;
+  padding: 2px 6px 2px 6px;
+  border-top: 1px solid #CCCCCC;
+  border-right: 1px solid #333333;
+  border-bottom: 1px solid #333333;
+  border-left: 1px solid #CCCCCC;
+    }
+    .matchScore {
+        font-family: arial;
+        text-align: center;
+        text-shadow: black 2px 2px 2px;
+        line-height: 0.8;
+    }
+    .matchScore .extraSmallYellow {
+        color: #808080;
+        font-size: 11px;
+    }
+    .matchScore .smallYellow {
+        color: #808080;
+        font-size: 14px;
+        font-weight: bold;
+    }
+    .matchScore .mediumYellow {
+        color: #808080;
+        font-size: 22px;
+        font-weight: bold;
+    }
+    .matchScore .largeYellow {
+        color: #808080;
+        font-size: 32px;
+        font-weight: bold;
+    }
+    .matchScore .extraLargeYellow {
+        color: #808080;
+        font-size: 64px;
+        font-weight: 900;
+    }
+    .matchScore .extraSmallWhite {
+        color: #808080;
+        font-size: 9px;
+        font-weight: 600;
+    }
+    .matchScore .smallWhite {
+        color: #808080;
+        font-size: 11px;
+        font-weight: 600;
+    }
+    .matchScore .mediumWhite {
+        color: #808080;
+        font-size: 22px;
+        font-weight: bold;
+    }
+    .activeMatchScore {
+        font-family: arial;
+        text-align: center;
+        text-shadow: black 2px 2px 2px;
+        line-height: 0.8;
+    }
+    .extraSmallYellow {
+        color: #f9f000;
+        font-size: 11px;
+    }
+    .activeMatchScore .smallYellow {
+        color: #f9f000;
+        font-size: 14px;
+        font-weight: bold;
+    }
+    .activeMatchScore .mediumYellow {
+        color: #f9f000;
+        font-size: 22px;
+        font-weight: bold;
+    }
+    .activeMatchScore .largeYellow {
+        color: #f9f000;
+        font-size: 32px;
+        font-weight: bold;
+    }
+    .activeMatchScore .extraLargeYellow {
+        color: #f9f000;
+        font-size: 64px;
+        font-weight: 900;
+    }
+    .activeMatchScore .extraSmallWhite {
+        color: white;
+        font-size: 9px;
+        font-weight: 600;
+    }
+    .activeMatchScore .smallWhite {
+        color: white;
+        font-size: 11px;
+        font-weight: 600;
+    }
+    .activeMatchScore .mediumWhite {
+        color: white;
+        font-size: 22px;
+        font-weight: bold;
+    }
+</style>"""
+
