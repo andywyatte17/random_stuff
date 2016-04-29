4869ae862596f6b1a3acd0e9af8aa18e1cd8aaac
Andrew Wyatt <andywyatte17@gmail.com>
2016-04-29 15:49:44 +0100

    darts_pml2016 : Converted script into single python-only - this is simpler and has more potential for optimisation.

--- python/darts/darts_pml2016.py
+++ python/darts/darts_pml2016.py
@@ -1,36 +1,66 @@
 #!/bin/python
+
 import os, sys
 from os import path
 from pprint import pprint
-#os.chdir(os.path.dirname(sys.argv[0]))
+import subprocess
 
-path_of_this_script = path.dirname(path.abspath(sys.argv[0]))
-tmp_darts = "/tmp/darts.txt"
-for x in ("{} > {}", "{} Barneveld >> {}", "{} Wade >> {}",
-          "{} Taylor >> {}", "{} Anderson >> {}",
-          "{} Thornton >> {}", "{} van Gerwen >> {}",
-          "{} Lewis >> {}", "{} Wright >> {}"):
-  cmd0 = "bash '{}' ".format(path.join(path_of_this_script, '_darts_pml2016.sh')) + x
-  cmd = cmd0.format(sys.argv[1], tmp_darts)
-  print(cmd)
-  os.system( cmd )
+TEST = R"""
 
-# filters lines from tmp_darts
-lines = [line.lstrip().rstrip() for line in open(tmp_darts, "r").readlines()]
-x = 0
-while x < len(lines):
-  if (x%2)==1 and lines[x][:5]=="https":
-    lines.insert(x, "???")
-    x -= 1
-  else:
-    x += 1
-  
-lines = [(lines[i],lines[i+1]) for i in range(0,len(lines),2)]
-lines = [x for x in lines if ("2016" in x[1]) and (" {}".format(sys.argv[1]) in x[1])]
+References
 
-for x in range(0,len(lines)):
-  print("{:3d} - {}\n      {}".format(x, lines[x][0], lines[x][1]))
+   Visible links
+   1. http://www.google.co.uk/search?q=hey&um=1&ie=UTF-8&hl=en&tbm=isch&source=og&sa=N&tab=wi
+   2. http://maps.google.co.uk/maps?q=hey&um=1&ie=UTF-8&hl=en&sa=N&tab=wl
+   3. https://play.google.com/?q=hey&um=1&ie=UTF-8&hl=en&sa=N&tab=w8
+   4. http://www.youtube.com/results?q=hey&um=1&ie=UTF-8&gl=GB&sa=N&tab=w1
+"""
+
+def subprocess_grab(args):
+  return subprocess.Popen(args, stdout=subprocess.PIPE).communicate()[0]
+
+def extract_youtube_http(x):
+  n = x.find("http")
+  yt = x.find("youtube.")
+  yt_watch = x.find("watch?")
+  if n < 0 or yt < 0 or yt_watch < 0: return None
+  return x[n:]
+
+output = ""
+#output = TEST
+week = sys.argv[1]
+start = 0
+user_agent = ""
+#SEARCH = "https://www.google.co.uk/search?q="
+SEARCH = "https://www.bing.com/search?q="
+
+# Run multiple searches to grab all possible links. This also extracts only the unique links.
+if not output or output=="":
+  for names in ("", "Barneveld", "Wade",
+                "Taylor", "Anderson",
+                "Thornton", "van Gerwen",
+                "Lewis", "Wright"):
+    search = SEARCH + "youtube premier league darts 2016 {} week {}".format(names, week)
+    got = subprocess_grab(["lynx", "-listonly", "-dump", search])
+    output += got
+output = [ extract_youtube_http(x) for x in output.split("\n") if extract_youtube_http(x) ]
+lines = list(set(output))
+
+def yt_title(yt_link):
+  return subprocess_grab(["python", "-m", "youtube_dl", "--get-title", yt_link])
+
+lines = [ (x, yt_title(x)) for x in lines ]
+lines = [ x for x in lines if ("2016" in x[1] and str(sys.argv[1]) in x[1]) ] 
+pprint(lines)
+sys.exit(0)
+
+# Show numbered list of links
+for n in range(0,len(lines)):
+  yt_link = lines[n][0]
+  print("{:3d} - {}\n\t{}".format(n, yt_link, lines[n][1]))
+
 print "Which videos? ",
+
 nums = [int(x.strip("\n")) for x in sys.stdin.readline().split(" ")]
 for x in nums:
   os.system('python -m youtube_dl --format 18 "{}"'.format(lines[x][0]))
