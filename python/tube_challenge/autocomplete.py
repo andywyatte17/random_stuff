b6b592dd36788e686c6756a8d7932403f051b0bd
Andrew Wyatt <andywyatte17@gmail.com>
2016-07-22 17:04:22 +0100

    tube_challenge : Added autocompletion code.

---
+++ python/tube_challenge/autocomplete.py
@@ -0,0 +1,65 @@
+import termios, os, sys
+
+def get_key():
+    fd = sys.stdin.fileno()
+    old = termios.tcgetattr(fd)
+    new = termios.tcgetattr(fd)
+    new[3] = new[3] & ~termios.ICANON & ~termios.ECHO
+    new[6][termios.VMIN] = 1
+    new[6][termios.VTIME] = 0
+    termios.tcsetattr(fd, termios.TCSANOW, new)
+    c = None
+    try:
+        c = os.read(fd, 1)
+    finally:
+        termios.tcsetattr(fd, termios.TCSAFLUSH, old)
+    return c
+
+def get_autocomplete_string(autocomplete_fn):
+  from sys import stdout
+  total = ""
+  tabText = None
+  tab = -1
+  while(True):
+    c = get_key()
+    # print(ord(c))
+    if c=="\n":
+      stdout.write("\n")
+      stdout.flush()
+      break
+    elif ord(c)==127:  # backspace
+      tabText = None
+      if total != "": total = total[0:-1]
+      stdout.write("\b")
+      stdout.flush()
+    elif c=="\t":
+      complete_list = autocomplete_fn(total)
+      if not complete_list : continue
+      tab += 1
+      if tab >= len(complete_list) : tab = 0
+      nTotal = len(total)
+      white = 40 - len(complete_list[tab])
+      stdout.write("\r" + complete_list[tab] + " "*white + "\r" + total)
+      stdout.flush()
+      tabText = complete_list[tab]
+    else:
+      tabText = None
+      tab = -1
+      total += c
+      white = 40 - len(total)
+      stdout.write("\r" + total + " "*white + "\b"*white)
+      stdout.flush()
+  return tabText if tabText else total 
+
+if __name__=="__main__":
+  from sys import stdout
+  print("Type in the underground station (tab to autocomplete): ")
+  def complete_fn(s):
+    if len(s)>=3 and s.lower().startswith("edg"):
+      return ["edgware", "edgware road 1", "edgware road 2"]
+    if len(s)>=3 and s.lower().startswith("hea"):
+      return ["heathrow 1-2-3", "heathrow 4", "heathrow 5"]
+    return None
+  
+  result = get_autocomplete_string(complete_fn)
+  print("Result = {}".format(result))
--- python/tube_challenge/tube_adventure.py
+++ python/tube_challenge/tube_adventure.py
@@ -1,12 +1,33 @@
 from data_js import StationData
 from pprint import pprint
 from sys import stdin
+from autocomplete import get_autocomplete_string
+
+class GetStation:
+  def __init__(self, stationData):
+    self.stations = [(x,x.lower()) for x in stationData.AllStations()]
+
+  def autocomplete_fn(self, test):
+    if len(test)<3:
+      return None
+    test = test.lower()
+    result = []
+    for x in self.stations:
+      if x[1].startswith(test):
+        result.append(x[0])
+    return result
+
+  def get(self):
+    return get_autocomplete_string( lambda x : self.autocomplete_fn(x) )
+
 
 stationData = StationData()
+getStation = GetStation(stationData)
+
 #print(stationData.routes)
+# print(stationData.AllStations())
 
-print(stationData.AllStations())
+print("Start?")
+print(getStation.get())
 
-#print("Start?")
-#station = stdin.readline()
-print(stationData.LookupStation("Edgware Road", True))
+# print(stationData.LookupStation("Edgware Road", True))
