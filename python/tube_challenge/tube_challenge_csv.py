de9faf84502e19fd81029b3d285bc62569131be1
Andrew Wyatt <andywyatte17@gmail.com>
2016-08-03 13:50:13 +0100

    tube_challenge: tube_challenge_csv.py has more out now and -1..-4 switch; new attempt4.py

--- python/tube_challenge/tube_challenge_csv.py
+++ python/tube_challenge/tube_challenge_csv.py
@@ -8,22 +8,33 @@ station_next,by_route (includes other),other_timing
 ...
 '''
 
+import importlib
 import data.data_js as data_js
 import data.data_routes as data_routes
-import my_routes.attempt2 as the_attempt
 from tabulate import tabulate
-
-attempt = the_attempt.attempt
 
 from StringIO import StringIO
 from pprint import pprint
 import sys
 
+the_attempt = None
 sio = None
 if len(sys.argv)>1:
-  sio = open(sys.argv[1], 'r')
+  if sys.argv[1]=='-1':
+    the_attempt = importlib.import_module('my_routes.attempt1')
+  elif sys.argv[1]=='-2':
+    the_attempt = importlib.import_module('my_routes.attempt2')
+  elif sys.argv[1]=='-3':
+    the_attempt = importlib.import_module('my_routes.attempt3')
+  elif sys.argv[1]=='-4':
+    the_attempt = importlib.import_module('my_routes.attempt4')
+  else:
+    sio = open(sys.argv[1], 'r')
 else:
-  sio = StringIO(attempt)
+  the_attempt = importlib.import_module('my_routes.attempt1')
+
+if the_attempt != None:
+  sio = StringIO(the_attempt.attempt)
 
 RouteTups = []
 for line in sio:
@@ -49,7 +60,7 @@ def TimeBetween(last, to, by):
       dt = abs(timings[tix] - timings[lix])
       return (dt, by, set(stations[min(lix,tix) : max(lix,tix)+1]))
   if by==None:
-    raise IndexError
+    raise RuntimeError("Unknown {} => {}".format(last, to))
   return TimeBetween(last, to, None)
 
 
@@ -76,7 +87,10 @@ ALL_STATIONS = all_stations.copy()
 #pprint(all_stations)
 last = None
 raw_time = 0
-result = [ ("at", "total_journey_time", "left", "done") ]
+total_time_3 = 0
+total_time_4 = 0
+result = [ ("#", "at", "total_journey_time", "left", "done", "sum@3", "sum@4") ]
+nCount = 1
 for x in RouteTups:
   to = x[0]
   by = None
@@ -85,19 +99,29 @@ for x in RouteTups:
     by = x[1]
     if by=="other":
       other_time = x[2]
-      raw_time += ParseTime(other_time)
+      rt = ParseTime(other_time)
+      raw_time += rt
+      total_time_3 += rt + 3
+      total_time_4 += rt + 4
     else:
       rt, actual_by, visited = TimeBetween(last, to, by)
       old_all_stations = all_stations.copy()
       for v in visited:
         all_stations.discard(v)
       raw_time += rt
+      total_time_3 += rt + 3
+      total_time_4 += rt + 4
+
+  def TFmt(mm): return "{:02d}:{:02d}".format(mm/60, mm%60)
+
   #if not last: print(to)
   #elif by=="other": print("{},{},{}".format(to, actual_by, other_time))
   #else: print("{},{}".format(to, actual_by))
-  result.append( (to, "{:02d}:{:02d}".format(raw_time/60, raw_time%60), \
-                  len(ALL_STATIONS) - len(all_stations), len(all_stations)) )
+  result.append( (nCount, to, TFmt(raw_time), \
+                  len(ALL_STATIONS) - len(all_stations), len(all_stations), \
+                  TFmt(total_time_3), TFmt(total_time_4)) )
   last = x[0]
+  nCount += 1
 
 print(tabulate(result))
 if len(all_stations)>0:
