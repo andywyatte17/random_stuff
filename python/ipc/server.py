fd3cc438b22e8ce4469fe8a5a2426b44bb49c1f6
Andy Wyatt <andywyatte17@gmail.com>
2016-12-19 14:30:37 +0000

    python > ipc : Updated server.py to use print function; reduces conn.recv(..) size because of problems with lack of flushing.

--- python/ipc/server.py
+++ python/ipc/server.py
@@ -1,11 +1,15 @@
 # Echo server program
+from __future__ import print_function
 import socket
+import turtle
 
 def flush_one(data):
     x = data.find('\n')
     if x>=0:
         cmd = data[:x+1]
-        print(cmd)
+        print(cmd, end='')
+        if cmd.startswith('turtle.'):
+            exec(cmd)
         return data[x+1:]
     return data
 
@@ -15,12 +19,13 @@ s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.bind((HOST, PORT))
 s.listen(1)
 conn, addr = s.accept()
-print 'Connected by', addr
+print('Connected by', addr)
 last = ""
 while 1:
-    data = conn.recv(1024)
+    data = conn.recv(8)
     if not data:
         last = flush_one(last+data)
         continue
+    conn.sendall(data)
     last = flush_one(last+data)
 conn.close()
