29b299ec5368e56940f9a890b3952c49e14b5aba
Andrew Wyatt <andywyatte17@gmail.com>
2017-03-10 09:00:21 +0000

    darts : Upgrade darts_pml2016.py script to 2017; added some unittest code and switches - now don't need to specify the week if current.

--- python/darts/darts_pml2017.py
+++ python/darts/darts_pml2017.py
@@ -5,7 +5,9 @@ from os import path
 from pprint import pprint
 import subprocess
 import urllib2
-import time, sys
+import time
+import argparse
+import datetime as dt
 
 TEST = R"""
 
@@ -17,6 +19,13 @@ References
    3. https://play.google.com/?q=hey&um=1&ie=UTF-8&hl=en&sa=N&tab=w8
    4. http://www.youtube.com/results?q=hey&um=1&ie=UTF-8&gl=GB&sa=N&tab=w1
 """
+
+def get_nearest_week(day_of_year = None):
+  FIRST_DAY = dt.datetime(2017, 2, 2, 0, 0, 0, 0).timetuple().tm_yday
+  if day_of_year==None:
+    day_of_year = dt.datetime.now().timetuple().tm_yday
+  n = (day_of_year - FIRST_DAY + 7)/7
+  return 1 if n<1 else n
 
 def subprocess_grab(args):
   return subprocess.Popen(args, stdout=subprocess.PIPE).communicate()[0]
@@ -30,9 +39,44 @@ def extract_youtube_http(x):
   if n < 0 or yt_watch < 0: return None
   return x[n:] if end<0 else x[n:end]
 
+# ...
+
+import unittest
+
+class Test(unittest.TestCase):
+  def setUp(self):
+    pass
+    
+  def test_get_nearest_week(self):
+    yd = lambda y_m_d : dt.datetime(y_m_d[0], y_m_d[1], y_m_d[2], 0, 0, 0, 0).timetuple().tm_yday
+    w = get_nearest_week( yd((2017,2,2)) )
+    self.assertEqual(w,1)
+    w = get_nearest_week( yd((2017,2,2+6)) )
+    self.assertEqual(w,1)
+    w = get_nearest_week( yd((2017,2,2+7)) )
+    self.assertEqual(w,2)
+    w = get_nearest_week( yd((2016,1,1)) )
+    self.assertEqual(w,1)
+
+# ...
+
+args_ = argparse.ArgumentParser(description='Get Premier League darts videos.')
+args_.add_argument('--week', dest='week', action='store_const',
+                   const=sum, default=-1,
+                   help='Choose week of Premier League to search for. Default = -1, current week')
+args_.add_argument('--test', dest='test', action='store_const',
+                   const=True, default=False,
+                   help='Run unit tests.')
+args = args_.parse_args()
+
+if args.test:
+  import sys
+  unittest.main(argv=sys.argv[:1], exit=True)
+
 output = ""
 #output = TEST
-week = sys.argv[1]
+week = args.week
+if week<0 : week = get_nearest_week()
 start = 0
 user_agent = ""
 SEARCH = "https://www.google.co.uk/search?q="
@@ -44,7 +88,7 @@ if not output or output=="":
                 "Taylor", "Anderson",
                 "Thornton", "van Gerwen",
                 "Lewis", "Wright"):
-    search = SEARCH + "youtube premier league darts 2016 {} week {}".format(names, week)
+    search = SEARCH + "youtube premier league darts 2017 {} week {}".format(names, week)
     got = subprocess_grab(["lynx", "-listonly", "-dump", search])
     output += got
 output = [ x for x in output.split("\n") ]
@@ -58,7 +102,7 @@ def yt_title(yt_link):
 
 from joblib import Parallel, delayed
 lines = Parallel(n_jobs=8, verbose=10)(delayed(yt_title)(i) for i in lines)
-lines = [ x for x in lines if ("2016" in x[1] and str(sys.argv[1]) in x[1]) ]
+lines = [ x for x in lines if ("2017" in x[1] and str(week) in x[1]) ]
 # pprint(lines)
 
 # Show numbered list of links
@@ -71,4 +115,3 @@ print "Which videos? ",
 nums = [int(x.strip("\n")) for x in sys.stdin.readline().split(" ")]
 for x in nums:
   os.system('python -m youtube_dl --restrict-filenames --format 18 "{}"'.format(lines[x][0]))
-
