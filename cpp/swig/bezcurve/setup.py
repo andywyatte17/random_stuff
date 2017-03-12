7944cd30ec7fa5c1254727b4f9a3d37b39dd60b6
Andrew Wyatt <andywyatte17@gmail.com>
2017-03-12 22:21:21 +0000

    cpp > swig > bezcurve : Finally got swig example to work (ie import bezcurve)

--- cpp/swig/bezcurve/setup.py
+++ cpp/swig/bezcurve/setup.py
@@ -1,8 +1,12 @@
 from distutils.core import setup, Extension
 
-extension_mod = Extension("bezcurve", ["_bezcurve_module.cpp", "bezcurve.cpp"])
+extension_mod = Extension("_bezcurve",
+                          sources=["bezcurve_wrap.cxx", "bezcurve.cpp"],
+                          )
 
-setup(name = "bezcurve", ext_modules=[extension_mod],
-                         swig_opts=['-c++'],
-                         )
+setup(name = "bezcurve",
+      ext_modules=[extension_mod],
+      py_modules = ["bezcurve"],
+      swig_opts=['-c++'],
+      )
 
