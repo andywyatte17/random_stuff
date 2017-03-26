1d25a2abf29d51b7f540e695480926e069740460
Andrew Wyatt <andyw@emnetsoftware.com>
2017-03-26 17:14:13 +0100

    blogs - Added stdafxize.py script to accompany cpp_build_opts.md

---
+++ blog/stdafxize.py
@@ -0,0 +1,47 @@
+#!/bin/python
+
+import os, sys
+from stdafx_h import *
+
+stdafx_cpp = R"""//
+// stdafx.cpp
+//
+"""
+
+def MakeStdafxPath(cppPath):
+    dirname = os.path.dirname(cppPath)
+    stdafx_h_loc = "stdafx.h"
+    for i in range(0,10):
+        if os.path.exists( os.path.join(dirname, stdafx_h_loc) ):
+            return stdafx_h_loc
+        stdafx_h_loc = "../" + stdafx_h_loc
+    raise RuntimeError("Whoops!")
+
+def AddStdafxHeaderToCpp(cppPath):
+    stdafx_h_loc = MakeStdafxPath(cppPath)
+    old_stdout = sys.stdout
+    import fileinput
+    found = False
+    for line in fileinput.input(cppPath, inplace=1):
+        if line.startswith('#include') and not found:
+            # old_stdout.write(cppPath)
+            if not ("stdafx.h" in line):
+                print('''#include "stdafx.h"'''.replace("stdafx.h", stdafx_h_loc))
+            print(line.replace("\n","").replace("\r",""))
+            found = True
+        else:
+            print(line.replace("\n","").replace("\r",""))
+
+project_root = sys.argv[1]
+os.chdir(project_root)
+if not open("stdafx.cpp", "r"):
+    open("stdafx.cpp", "w").write( stdafx_cpp )
+if not open("stdafx.h", "r"):
+    open("stdafx.h", "w").write( stdafx_h )
+
+for root, dirs, files in os.walk('.'):
+    for x in [os.path.join(root, name) for name in files]:
+        if not x.endswith(".cpp"): continue
+        if "Tests\\" in x: continue
+        if "stdafx" in x: continue
+        AddStdafxHeaderToCpp(x)
