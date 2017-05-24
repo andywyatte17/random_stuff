35a7e8a83594845f27178bc80b45937afa662320
Andrew Wyatt <andywyatte17@gmail.com>
2017-05-24 23:32:59 +0100

    python > underground : Further lines defined.

--- python/underground/gps.py
+++ python/underground/gps.py
@@ -1,14 +1,14 @@
-import csv, os
+import csv, os, sys
 import turtle
 import line_list
-import name_lookup
+from name_lookup import convert as nlconv
 
 def from_csv(filename):
     stations = []
     with open(filename, 'r') as f:
         r = csv.reader(f)
         for row in r:
-            stations += row
+            stations.append(row[0])
     return stations
 
 vic = from_csv('victoria_line.csv')
@@ -16,6 +16,8 @@ nor_bank = from_csv('northern_bank.csv')
 nor_charing_x = from_csv('northern_charing_x.csv')
 central = from_csv('central_line.csv')
 central_hainault = from_csv('central_hainault.csv')
+metro_amersham = from_csv('metro_amersham.csv')
+metro_watford = from_csv('metro_watford.csv')
 
 gps = dict()
 r = csv.reader(open('gps.csv', 'r'))
@@ -27,24 +29,36 @@ lat_max = max(gps.values(), key = lambda x: x[0])[0]
 long_min = min(gps.values(), key = lambda x: x[1])[1]
 long_max = max(gps.values(), key = lambda x: x[1])[1]
 
+W,H=450,350
+turtle.color(0.9, 0.9, 0.9)
+turtle.penup()
+for x,y in ((-1,-1),(1,-1),(1,1),(-1,1),(-1,-1)):
+  turtle.setposition(x*W*0.5,y*H*0.5)
+  turtle.pendown()
+
 turtle.speed(1)
 for line, col in ( (vic, (0,0.5,1.0)),
                    (nor_bank, (0,0,0)),
                    (nor_charing_x, (0,0,0)),
                    (central, (1,0,0)),
-                   (central_hainault, (1,0,0))
+                   (central_hainault, (1,0,0)),
+                   (metro_watford, (1,0,1)),
+                   (metro_amersham, (1,0,1))
                  ):
     turtle.penup()
     turtle.width(1.5)
     turtle.color(col)
     for st in line:
+        st = nlconv(st)
         if st in gps:
             latt, longt = gps[st]
             x = (longt - long_min)/(long_max-long_min)
             y = (latt - lat_min)/(lat_max-lat_min)
-            x *= 200
-            y *= 200
+            x -= 0.5
+            y -= 0.5
+            x = x*W
+            y = y*H
             turtle.setposition(x, y)
-            #print(st)
             turtle.pendown()
-        else: print("Missing - " + st)
+        else:
+            print(st)
--- python/underground/name_lookup.py
+++ python/underground/name_lookup.py
@@ -1,3 +1,14 @@
 NAME_LOOKUP = {
-  "Hammersmith":"h"
+  "King's Cross St Pancras":"King's Cross St.Pancras",
+  "South Wimbledon (Merton)":"South Wimbledon",
+  "Totteridge and Whetstone":"Totteridge & Whetstone",
+  "Euston Underground Station":"Euston",
+  "St Paul's":"St.Paul's"
 }
+
+OFFICIAL_NAMES = set(("King's Cross St.Pancras",))
+
+def convert(n):
+  if n in NAME_LOOKUP:
+    return NAME_LOOKUP[n]
+  return n
